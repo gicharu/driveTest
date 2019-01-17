4951c22189cfd9b7f0c22f28074d4875447a8b29
James Njoroge <james@mnemonicssystems.co.ke>
2019-01-17 20:59:30 +0300

    Adjust config

--- common/models/User.php
+++ common/models/User.php
@@ -1,11 +1,12 @@
 <?php
 namespace common\models;
 
-use Yii;
-use yii\base\NotSupportedException;
-use yii\behaviors\TimestampBehavior;
-use yii\db\ActiveRecord;
+// use Yii;
+// use yii\base\NotSupportedException;
+// use yii\behaviors\TimestampBehavior;
+// use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
+use dektrium\user\models\User as BaseUserModel;
 
 /**
  * User model
@@ -21,7 +22,7 @@ use yii\web\IdentityInterface;
  * @property integer $updated_at
  * @property string $password write-only password
  */
-class User extends ActiveRecord implements IdentityInterface
+class User extends BaseUserModel implements IdentityInterface
 {
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
@@ -30,47 +31,47 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * {@inheritdoc}
      */
-    public static function tableName()
-    {
-        return '{{%user}}';
-    }
+//     public static function tableName()
+//     {
+//         return '{{%user}}';
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public function behaviors()
-    {
-        return [
-            TimestampBehavior::className(),
-        ];
-    }
+//     public function behaviors()
+//     {
+//         return [
+//             TimestampBehavior::className(),
+//         ];
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public function rules()
-    {
-        return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
-        ];
-    }
+//     public function rules()
+//     {
+//         return [
+//             ['status', 'default', 'value' => self::STATUS_ACTIVE],
+//             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+//         ];
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentity($id)
-    {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
-    }
+//     public static function findIdentity($id)
+//     {
+//         return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
-    }
+//     public static function findIdentityByAccessToken($token, $type = null)
+//     {
+//         throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+//     }
 
     /**
      * Finds user by username
@@ -78,10 +79,10 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $username
      * @return static|null
      */
-    public static function findByUsername($username)
-    {
-        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
-    }
+//     public static function findByUsername($username)
+//     {
+//         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+//     }
 
     /**
      * Finds user by password reset token
@@ -89,17 +90,17 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $token password reset token
      * @return static|null
      */
-    public static function findByPasswordResetToken($token)
-    {
-        if (!static::isPasswordResetTokenValid($token)) {
-            return null;
-        }
+//     public static function findByPasswordResetToken($token)
+//     {
+//         if (!static::isPasswordResetTokenValid($token)) {
+//             return null;
+//         }
 
-        return static::findOne([
-            'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
-        ]);
-    }
+//         return static::findOne([
+//             'password_reset_token' => $token,
+//             'status' => self::STATUS_ACTIVE,
+//         ]);
+//     }
 
     /**
      * Finds out if password reset token is valid
@@ -107,40 +108,40 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $token password reset token
      * @return bool
      */
-    public static function isPasswordResetTokenValid($token)
-    {
-        if (empty($token)) {
-            return false;
-        }
+//     public static function isPasswordResetTokenValid($token)
+//     {
+//         if (empty($token)) {
+//             return false;
+//         }
 
-        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
-        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
-        return $timestamp + $expire >= time();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->getPrimaryKey();
-    }
+//         $timestamp = (int) substr($token, strrpos($token, '_') + 1);
+//         $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+//         return $timestamp + $expire >= time();
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public function getAuthKey()
-    {
-        return $this->auth_key;
-    }
+//     public function getId()
+//     {
+//         return $this->getPrimaryKey();
+//     }
 
     /**
      * {@inheritdoc}
      */
-    public function validateAuthKey($authKey)
-    {
-        return $this->getAuthKey() === $authKey;
-    }
+//     public function getAuthKey()
+//     {
+//         return $this->auth_key;
+//     }
+
+    /**
+     * {@inheritdoc}
+     */
+//     public function validateAuthKey($authKey)
+//     {
+//         return $this->getAuthKey() === $authKey;
+//     }
 
     /**
      * Validates password
@@ -148,42 +149,42 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $password password to validate
      * @return bool if password provided is valid for current user
      */
-    public function validatePassword($password)
-    {
-        return Yii::$app->security->validatePassword($password, $this->password_hash);
-    }
+//     public function validatePassword($password)
+//     {
+//         return Yii::$app->security->validatePassword($password, $this->password_hash);
+//     }
 
     /**
      * Generates password hash from password and sets it to the model
      *
      * @param string $password
      */
-    public function setPassword($password)
-    {
-        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
-    }
+//     public function setPassword($password)
+//     {
+//         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+//     }
 
     /**
      * Generates "remember me" authentication key
      */
-    public function generateAuthKey()
-    {
-        $this->auth_key = Yii::$app->security->generateRandomString();
-    }
+//     public function generateAuthKey()
+//     {
+//         $this->auth_key = Yii::$app->security->generateRandomString();
+//     }
 
     /**
      * Generates new password reset token
      */
-    public function generatePasswordResetToken()
-    {
-        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
-    }
+//     public function generatePasswordResetToken()
+//     {
+//         $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+//     }
 
     /**
      * Removes password reset token
      */
-    public function removePasswordResetToken()
-    {
-        $this->password_reset_token = null;
-    }
+//     public function removePasswordResetToken()
+//     {
+//         $this->password_reset_token = null;
+//     }
 }
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,10 +44,10 @@ AppAsset::register($this);
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/user/login']];
     } else {
         $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
+            . Html::beginForm(['/user/logout'], 'post')
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link logout']
