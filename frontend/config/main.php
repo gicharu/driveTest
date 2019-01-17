1c8d1f247c970369079b755c9c01654dca90583c
James Njoroge <james@mnemonicssystems.co.ke>
2019-01-17 20:48:18 +0300

    Adjust config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -16,8 +16,8 @@ return [
             'csrfParam' => '_csrf-frontend',
         ],
         'user' => [
-//             'identityClass' => 'common\models\User',
-            'identityClass' => 'app\components\User',
+            'identityClass' => 'common\models\User',
+//             'identityClass' => 'app\components\User',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
         ],
